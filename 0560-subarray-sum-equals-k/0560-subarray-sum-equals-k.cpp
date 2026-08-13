32b40c9ce0c098524e4a716d8e8b1e21cd92c50f
Pankaj Shah <pankajshah9685@gmail.com>
2026-08-13 21:13:56 +0530

    Time: 48 ms (46.04%), Space: 51.1 MB (14.13%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -5,8 +5,8 @@ public:
         int prefixSum = 0;
         mp[0] = 1;
         int result = 0;
-        for(int i = 0; i < nums.size(); i++){
-            prefixSum+=nums[i];
+        for(int value : nums){
+            prefixSum+=value;
             result+=mp[prefixSum-k];
             mp[prefixSum]++;
         }
