a56b32f03f28d701b0b2c30e3311a6fc3e930a16
Pankaj Shah <pankajshah9685@gmail.com>
2026-08-27 11:26:20 +0530

    Time: 0 ms (100%), Space: 10 MB (11.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        if(n != m){
-            return 0;
-        }
-        vector<int>freq(26,0);
-        for(char  x : s){
-            freq[x - 'a']++;
-        }
-        for(char  x : t){
-            freq[x - 'a']--;
-        }
-        for(int x : freq){
-            if(x != 0){
-                return 0;
-            }
-        }
-        return 1;
+        unordered_map<char, int> freq1, freq2;
+
+        for (char c : s) freq1[c]++;
+        for (char c : t) freq2[c]++;
+
+        return freq1 == freq2;
     }
 };
