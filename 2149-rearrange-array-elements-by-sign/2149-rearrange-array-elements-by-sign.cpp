30c599b5a8ceb4afd08b5023e7f57594c5ed368b
Pankaj Shah <pankajshah9685@gmail.com>
2026-09-02 15:46:37 +0530

    Time: 15 ms (11.55%), Space: 144.7 MB (11.61%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -3,24 +3,21 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n = nums.size();
         vector<int>sum;
-        vector<int>sub;
+        // vector<int>sub;
         for(int  i = 0; i < n; i++){
             if(nums[i] > 0){
                 sum.push_back(nums[i]);
-            }else{
-                sub.push_back(nums[i]);
             }
         }
-      
-        // for(int  i = 0; i < n; i++){
-        //     if(nums[i] < 0){
-        //         sub.push_back(nums[i]);
-        //     }
-        // }
+        for(int  i = 0; i < n; i++){
+            if(nums[i] < 0){
+                sum.push_back(nums[i]);
+            }
+        }
         vector<int>result;
         for(int i = 0; i < n/2; i++){
             result.push_back(sum[i]);
-            result.push_back(sub[i]);
+            result.push_back(sum[n/2 + i]);
         }
         return result;
     }
