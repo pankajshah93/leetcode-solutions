d6d47f55fe0c590378fd959f6cb8931a4b79a38a
Pankaj Shah <pankajshah9685@gmail.com>
2026-09-03 17:56:50 +0530

    Time: 0 ms (100%), Space: 23.7 MB (65.84%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        int size = n/3;
-        vector<int>result;
-        unordered_map<int,int>mp;
-        for(int x : nums){
-            mp[x]++;
-        }
-        for(auto x : mp){
-            if(x.second > size){
-                result.push_back(x.first);
+        //only give two answer  n/3  not grater and graetar than n/3 so only two value in answer
+        int count1 = 0,count2 = 0;
+        int ele1 = INT_MIN,ele2 = INT_MIN;
+        for(int i = 0; i < nums.size(); i++){
+            if(count1 == 0 && ele2 != nums[i]){
+                ele1 = nums[i];
+                count1++;
+            }else if(count2 == 0 && ele1 != nums[i]){
+                ele2 = nums[i];
+                count2++;
+            }else if(ele1 == nums[i]) count1++;
+            else if(ele2 == nums[i]) count2++;
+            else{
+                count1--;
+                count2--;
             }
         }
-        return result;
+        vector<int>ls;
+        int cnt1 = 0,cnt2 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(ele1 == nums[i]) cnt1++;
+            if(ele2 == nums[i])  cnt2++;
+        }
+        int Min = (int)(nums.size()/3) + 1;
+        if(cnt1 >= Min) ls.push_back(ele1);
+        if(cnt2 >= Min) ls.push_back(ele2);
+        sort(nums.begin(),nums.end());
+        return ls;;
     }
-    
 };
